b4da52ee657141c72b356de7176c051bf7d7fbe2
James Addison <addi00+github.com@gmail.com>
2017-03-27 13:37:32 -0700

    Fixes to allow more flexible user authentication scenarios.

--- simple_socialauth/backends.py
+++ simple_socialauth/backends.py
@@ -2,20 +2,34 @@ from __future__ import unicode_literals
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend as StandardModelBackend
 
+from .models import SocialAccount
+
 UserModel = get_user_model()
 
 
 class Backend(StandardModelBackend):
-    def authenticate(self, pk=None, social=False, **kwargs):
+    def authenticate(self, social=False, **kwargs):
         if social:
-            try:
-                user = UserModel.objects.get(pk=pk)
-            except UserModel.DoesNotExist:
-                # Run the default password hasher once to reduce the timing
-                # difference between an existing and a nonexistent user (#20760).
-                UserModel().set_password(None)
-            else:
-                # `user_can_authenticate` only appeared in Django 1.10, so
-                # ignore it if it doesn't exist for lower versions' sakes
-                if not hasattr(self, 'user_can_authenticate') or self.user_can_authenticate(user):
-                    return user
+            uid = kwargs.get('uid', '')
+            provider = kwargs.get('provider', '')
+            if uid and provider:
+                try:
+                    return SocialAccount.objects.select_related('user').get(
+                        uid=uid, provider=provider
+                    ).user
+                except SocialAccount.DoesNotExist:
+                    pass
+
+            pk = kwargs.get('pk')
+            if pk:
+                try:
+                    user = UserModel.objects.get(pk=pk)
+                except UserModel.DoesNotExist:
+                    # Run the default password hasher once to reduce the timing
+                    # difference between an existing and a nonexistent user (#20760).
+                    UserModel().set_password(None)
+                else:
+                    # `user_can_authenticate` only appeared in Django 1.10, so
+                    # ignore it if it doesn't exist for lower versions' sakes
+                    if not hasattr(self, 'user_can_authenticate') or self.user_can_authenticate(user):
+                        return user
