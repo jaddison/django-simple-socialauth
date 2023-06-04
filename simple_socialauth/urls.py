2deb35c911499b56c90434932f5b1575ab366985
Kiefna <chris.candell@chatterblock.com>
2023-06-03 17:14:28 -0700

    Replace url with new re_path variable for Django 3.2

--- simple_socialauth/urls.py
+++ simple_socialauth/urls.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 
 urlpatterns = [
-    path(r'^(?P<provider_type>[\w\-]+)/login/$', views.LoginView.as_view(), name='simple_socialauth-login'),
-    path(r'^(?P<provider_type>[\w\-]+)/callback/$', views.CallbackView.as_view(), name='simple_socialauth-login_callback'),
-    path(r'^(?P<provider_type>[\w\-]+)/complete/$', views.CompleteView.as_view(), name='simple_socialauth-login_complete'),
+    re_path(r'^(?P<provider_type>[\w\-]+)/login/$', views.LoginView.as_view(), name='simple_socialauth-login'),
+    re_path(r'^(?P<provider_type>[\w\-]+)/callback/$', views.CallbackView.as_view(), name='simple_socialauth-login_callback'),
+    re_path(r'^(?P<provider_type>[\w\-]+)/complete/$', views.CompleteView.as_view(), name='simple_socialauth-login_complete'),
 ]
